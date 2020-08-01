5026b923cca6f07c0ee8635edcafb51ab0e30b10
IG-88-2 <anatoly.strashkevich@gmail.com>
2020-08-01 08:58:00 +0300

    dev

---
+++ rollup.config.ts
@@ -0,0 +1,53 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
+
+const globals = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+  'prop-types': 'PropTypes',
+};
+
+const external = [
+  'react',
+  'react-dom',
+  'prop-types',
+];
+
+export default {
+  input: `react-janus-videoroom.tsx`,
+  output: [
+    {
+      name: 'react-janus-videoroom', 
+      file: pkg.browser,
+      format: 'umd',
+      sourcemap: false,
+      globals
+    },
+    {
+      name: 'react-janus-videoroom', 
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: false,
+      globals
+    },
+    {
+      name: 'react-janus-videoroom', 
+      file: pkg.module,
+      format: 'es',
+      sourcemap: false,
+      globals
+    }
+  ],
+  external,
+  plugins: [
+    typescript({ useTsconfigDeclarationDir: true }),
+    commonjs(),
+    resolve({
+      browser: true
+    }),
+    sourceMaps()
+  ]
+}
