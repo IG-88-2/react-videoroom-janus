1b939e9088145d1ea9ac84bfd14f310a4e5efad5
IG-88-2 <anatoly.strashkevich@gmail.com>
2020-08-05 10:50:14 +0300

    dev

--- dist/react-janus-videoroom.d.ts
+++ dist/react-janus-videoroom.d.ts
@@ -1,1 +1,33 @@
+import { Component } from 'react';
+interface JanusVideoRoomProps {
+    server: string;
+    room: any;
+    onConnected: (publisher: any) => void;
+    onDisconnected: (error: any) => void;
+    onRooms: (rooms: any) => void;
+    onParticipantJoined: (participant: any) => void;
+    onParticipantLeft: (participant: any) => void;
+    renderVideo: (video: any) => any;
+    renderLocalVideo: (localVideo: any) => any;
+    renderContainer: (children: any) => any;
+    renderStream: (stream: any) => any;
+    renderLocalStream: (localStream: any) => void;
+    customStyles: {
+        video?: any;
+        container?: any;
+        videoContainer?: any;
+        localVideo?: any;
+        localVideoContainer?: any;
+    };
+}
+interface JanusVideoRoomState {
+}
+export declare class JanusVideoRoom extends Component<JanusVideoRoomProps, JanusVideoRoomState> {
+    constructor(props: any);
+    componentDidMount(): void;
+    componentWillUnmount(): void;
+    componentWillReceiveProps(): void;
+    componentDidUpdate(): void;
+    render(): JSX.Element;
+}
 export {};
--- react-janus-videoroom.tsx
+++ react-janus-videoroom.tsx
@@ -1,20 +1,21 @@
-import client from 'janus-gateway-client';
-import ReconnectingWebSocket from 'reconnecting-websocket'; //TODO is this going to work with standart websocket ?
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { useState, useRef } from 'react';
+import ReconnectingWebSocket from 'reconnecting-websocket';
+import client from 'janus-gateway-client';
+import { Component, Fragment } from 'react';
 const { JanusClient } = client;
-console.log(JanusClient);
 
 let count = 0;
 
-const uuidv1 = () => {
+
+
+const getId = () => {
 
     count++;
 
     return String(count);
 
 }
+
 
 
 let enabled = false;
@@ -35,35 +36,35 @@ const logger = {
     success: (...args) => {
 
         if (enabled) {
-            
+            console.log(...args);
         }
 
     },
     info: (...args) => {
 
         if (enabled) {
-            
+            console.log(...args);
         }
 
     },
     error: (error:any) => {
 
         if (enabled) {
-            
+            console.error(error);
         }
 
     },
     json: (...args) => {
 
         if (enabled) {
-            
+            console.log(...args);
         }
 
     },
     tag: (tag:string, type:`success` | `info` | `error`) => (...args) => {
 
         if (enabled) {
-            
+            console.log(tag, type, ...args);
         }
         
     }
@@ -71,32 +72,224 @@ const logger = {
 
 
 
-const onPublisher = (publisher, onDisconnected) => {
+interface VideoProps {
+	id:string,
+	muted:boolean,
+	style:any,
+	stream:any
+}
 
-	const video = document.createElement("video");
 
-	video.id = publisher.id;
-	video.autoplay = true;
-	video.muted = true;
-	video.width = 320;
-	video.height = 240;
-	video.style.height = "100%";
-	
-	const container = document.getElementById("local");
-	
-	container.appendChild(video);
 
-	publisher.addEventListener("terminated", () => {
+interface VideoState {
+
+}
+
+
+
+class Video extends Component<VideoProps,VideoState> {
+	video:any
+
+	constructor(props) {
+
+		super(props);
 		
-		const video = document.getElementById(publisher.id);
+    }
 
+	componentDidMount() {
+
+		this.video.srcObject = this.props.stream;
+
+	}
+
+	render () {
+
+		const {
+			id,
+			muted,
+			style
+		} = this.props;
+
+		return <video
+			id={id}
+			muted={muted}
+			autoPlay
+			style={style}
+			ref={(video) => { this.video = video; }}
+		/>
+
+	}
+
+}
+
+
+
+interface CustomStyles {
+	video?:any,
+	container?:any,
+	videoContainer?:any,
+	localVideo?:any,
+	localVideoContainer?:any
+}
+
+interface JanusVideoRoomProps {
+	server:string,
+	room:string,
+	onConnected:(publisher:any) => void,
+	onDisconnected:(error?:any) => void,
+	onRooms:(rooms:any) => void,
+	onError:(error:any) => void,
+	onParticipantJoined:(participant:any) => void,
+	onParticipantLeft:(participant:any) => void,
+	renderContainer:(children:any) => any,
+	renderStream:(subscriber:any) => any,
+	renderLocalStream:(publisher:any) => any,
+	customStyles:CustomStyles
+}
+
+
+
+interface JanusVideoRoomState {
+
+}
+
+
+
+export class JanusVideoRoom extends Component<JanusVideoRoomProps,JanusVideoRoomState> {
+	client:any 
+	connected:boolean
+	styles:CustomStyles
+
+    constructor(props) {
+
+        super(props);
+
+		this.state = {};
+		
+		const customStyles = this.props.customStyles || {};
+
+		this.styles = {
+			video:{
+
+			},
+			container:{
+				
+			},
+			videoContainer:{
+				
+			},
+			localVideo:{
+				
+			},
+			localVideoContainer:{
+				
+			},
+			...customStyles
+		};
+
+    }
+
+
+
+    componentDidMount() {
+
+		const { server } = this.props;
+		
+		this.client = new JanusClient({
+			server,
+			logger,
+			WebSocket: ReconnectingWebSocket,
+			onPublisher: this.onPublisher,
+			onSubscriber: this.onSubscriber,
+			onError: (error) => {
+	
+				this.props.onError(error);
+	
+			},
+			getId: () => getId()
+		});
+	
+		this.client.initialize()
+		.then(() => (
+	
+			this.client.getRooms()
+	
+		))
+		.then(({ load }) => {
+
+			this.props.onRooms(load);
+
+			this.connected = true;
+
+		})
+		.catch((error) => {
+
+			this.props.onError(error);
+
+		});
+
+    }
+
+
+
+	componentDidUpdate(prevProps:JanusVideoRoomProps) {
+
+		if (prevProps.room!==this.props.room && this.props.room) {
+			this.changeRoom(prevProps);
+		}
+
+	}
+
+
+
+	componentWillUnmount() {
+
+		this.connected = false;
+
+		this.client.terminate()
+		.then(() => {
+			
+			this.props.onDisconnected();
+
+		})
+		.catch((error) => {
+
+			this.props.onError(error);
+
+		});
+						
+	}
+	
+
+
+	changeRoom = (prevProps:JanusVideoRoomProps) => {
+
+		if (prevProps.room) {
+			this.client.leave()
+			.then(() => {
+				this.client.join(this.props.room);
+			});
+		} else {
+			this.client.join(this.props.room);
+		}
+
+	}
+
+
+
+	onPublisherTerminated = (publisher) => () => {
+
+		const video = document.getElementById(publisher.id);
+	
 		if (video) {
 			video.remove();
 		}
 
-	});
+	}
 
-	publisher.addEventListener("disconnected", () => {
+
+
+	onPublisherDisconnected = (publisher) => () => {
 		
 		//TODO where i should reinitialize in case webrtc down ???
 		//TODO prevent any actions while renegotiation is happening
@@ -110,528 +303,192 @@ const onPublisher = (publisher, onDisconnected) => {
 			video.srcObject = publisher.stream;
 
 		});
+
+		const room_id = client.current.room_id;
+
+		client.current.leave()
+		.then(() => {
+
+
+			return client.current.join(room_id);
+
+		})
+		.then(() => {
+
+			
+		})
+		.catch((error) => {
+
+
+		});
 		*/
-	});
+
+	}
+
+
+
+	onPublisher = async (publisher) => {
+
+		publisher.addEventListener("terminated", this.onPublisherTerminated(publisher));
 	
-	video.srcObject = publisher.stream;
+		publisher.addEventListener("disconnected", this.onPublisherDisconnected(publisher));
 
-};
-
-
-
-const onSubscriber = async (subscriber) => {
-
-	const video = document.createElement("video");
-
-	video.id = subscriber.id;
-	video.autoplay = true;
-	video.width = 180;
-	video.height = 120;
-	video.style.background = "green";
-	video.style.padding = "5px";
+		this.props.onConnected(publisher);
 	
-	const container = document.getElementById("container");
+		const video = document.createElement("video");
 
-	container.appendChild(video);
-
-	subscriber.addEventListener("terminated", () => {
-
-        
-		const video = document.getElementById(subscriber.id);
-
-		if (video) {
-			video.remove();
-		}
-
-	});
-
-	subscriber.addEventListener("leaving", () => {
-
-        
-		const video = document.getElementById(subscriber.id);
-
-		if (video) {
-			video.remove();
-		}
-
-	});
-
-	subscriber.addEventListener("disconnected", () => {
-        
-
-        
-	});
-
-	await subscriber.initialize();
-	
-	video.srcObject = subscriber.stream;
-
-};
-
-
-
-const onDisconnected = (client) => {
-
-	const room_id = client.current.room_id;
-
-	client.current.leave()
-	.then(() => {
-
-
-		return client.current.join(room_id);
-
-	})
-	.then(() => {
-
+		video.id = publisher.id;
+		video.autoplay = true;
+		video.muted = true;
+		video.width = 320;
+		video.height = 240;
+		video.style.height = "100%";
 		
-	})
-	.catch((error) => {
+		const container = document.getElementById("local");
+		
+		container.appendChild(video);
+	
+		
+		
+		video.srcObject = publisher.stream;
 
-
-	});
-
-};
-
-
-
-const connect = (client, server) => {
-
-	if (client.current) {
-		console.log('already connected...');
-		return client.current.getRooms().then(({ load }) => load);
 	}
 
-	client.current = new JanusClient({
-		server,
-		logger,
-		WebSocket: ReconnectingWebSocket,
-		onPublisher: (publisher) => {
-
-			onPublisher(publisher, onDisconnected);
-
-		},
-		onSubscriber: async (subscriber) => {
-			
-			onSubscriber(subscriber);
-
-		},
-		onError: (error) => {
-
-            
-
-		},
-		getId: () => uuidv1()
-	});
-
-	return client.current.initialize()
-	.then(() => (
-
-		client.current.getRooms().then(({ load }) => load)
-
-	));
-
-};
 
 
+	onSubscriberTerminater = (subscriber) => () => {
+		
+		const video = document.getElementById(subscriber.id);
 
-const disconnect = async (client) => {
-
-	if (client.current) {
-		if (client.current.publisher) {
-			const video = document.getElementById(client.current.publisher.id);
-			
-			if (video) {
-				video.remove();
-			}
+		if (video) {
+			video.remove();
 		}
 
-		try {
-			await client.current.terminate();
-			console.log('terminated!');
-		} catch(error) {
-			console.error(error);
+		this.props.onParticipantLeft(subscriber);
+
+	}
+
+
+
+	onSubscriberLeaving = (subscriber) => () => {
+		
+		const video = document.getElementById(subscriber.id);
+
+		if (video) {
+			video.remove();
 		}
 
-		client.current = null;
+		this.props.onParticipantLeft(subscriber);
+			
 	}
 
-};
+
+
+	onSubscriberDisconnected = (subscriber) => () => {
+		
+		
+			
+	}
 
 
 
-const onCreateRoom = (client, description:string) => {
+	onSubscriber = async (subscriber) => {
+		
+		subscriber.addEventListener("terminated", this.onSubscriberTerminater(subscriber));
 	
-	return client.current.createRoom(description);
-
-};
-
-
-
-const onJoinRoom = (client, room) => {
-
-	if (client.current) {
-		return client.current.join(room.room_id);
-	}
-
-};
-
-
-
-const onLeaveRoom = (client, room) => {
-
-	if (client.current) {
-		return client.current.leave();
-	}
+		subscriber.addEventListener("leaving", this.onSubscriberLeaving(subscriber));
 	
-};
+		subscriber.addEventListener("disconnected", this.onSubscriberLeaving(subscriber));
+		
+		await subscriber.initialize();
+
+		this.props.onParticipantJoined(subscriber);
+		
+	}
 
 
 
-const Room = ({ room, onJoin, onLeave } : any) => {
+	renderVideo = (subscriber) => {
 
-	const {
-		room_id,
-		instance_id
-	} = room;
+		if (this.props.renderStream) {
+			return this.props.renderStream(subscriber);
+		}
 
-	return (
-		<div 
-			className="room-element"
-			key={room_id}
-			style={{
-				display:"flex",
-				flexDirection:"column",
-				padding:"10px",
-				color:"white",
-				background:"red"
-			}}
+		return <div style={this.styles.videoContainer}>
+			<Video
+				id={subscriber.id}
+				muted={false}
+				style={this.styles.video}
+				stream={subscriber.stream}
+			/>
+		</div>
+		
+	}
+
+
+
+	renderLocalVideo = () => {
+
+		const publisher = this.client.publisher;
+
+		if (this.props.renderLocalStream(publisher)) {
+			return this.props.renderLocalStream(publisher);
+		}
+
+		return <div style={this.styles.localVideoContainer}>
+			<Video
+				id={publisher.id}
+				muted={true}
+				style={this.styles.localVideo}
+				stream={publisher.stream}
+			/>
+		</div>
+
+	}
+
+	
+
+	renderContainer = () => {
+		
+		const subscribers = Object.values(this.client.subscribers);
+
+		const content = (
+			<Fragment>
+				{
+					this.renderLocalVideo()
+				}
+				{
+					subscribers.map((subscriber) => {
+						
+						return this.renderVideo(subscriber);
+
+					})
+				}
+			</Fragment>
+		)
+
+		if (this.props.renderContainer) {
+			return this.props.renderContainer(content);
+		}
+
+		return <div
+			style={this.styles.container}
 		>
-			<div>
-				{instance_id}
-			</div>
-			<div className="room-id">
-				{room_id}
-			</div>
-			<div style={{
-				display:"flex"
-			}}>
-				<button 
-					id={`join-${room_id}`}
-					onClick={(e) => onJoin()}
-				>
-					Join
-				</button>
-				<button
-					id={`leave-${room_id}`}
-					onClick={(e) => onLeave()}
-				>
-					Leave
-				</button>
-			</div>
-		</div>
-	);
-
-};
-
-
-
-const VideoRoom = ({ server }) => {
-
-	const client = useRef(null);
-
-	const [rooms, setRooms] = useState([]);
-
-	const [muted, setMuted] = useState(false);
-
-	const [paused, setPaused] = useState(false);
-
-	const [description, setDescription] = useState(``);
-
-
-
-	return (
-		<div style={{
-			height:`100%`,
-			width:`100%`,
-			display:`flex`,
-			justifyContent:`space-between`,
-			background:`aliceblue`
-		}}>
-
-			<div style={{
-				display:`flex`,
-				flexDirection:`column`,
-				height:`100%`,
-				width:`33.3%`,
-				background:`beige`
-			}}>
-
-				<div style={{
-					display:`flex`
-				}}>
-					<button 
-						id="connect"
-						onClick={() => {
-
-							connect(client, server)
-							.then((rooms) => {
-								
-								setRooms(rooms);
-
-							});
-
-						}}
-					>
-						Connect
-					</button>
-					<button 
-						id="disconnect"
-						onClick={() => {
-
-							disconnect(client);
-							
-							setRooms([]);
-
-						}}
-					>
-						Disconnect
-					</button>
-				</div>
-
-				<div style={{
-					display:`flex`,
-					flexDirection:`column`
-				}}>
-					<input value={description} onChange={(e) => setDescription(e.target.value)} />
-					<button onClick={(e) => {
-
-						onCreateRoom(client, description)
-						.then((result) => {
-
-							logger.info('onCreateRoom response');
-							
-							logger.json(result);
-
-							return client.current.getRooms().then(({ load }) => load);
-
-						})
-						.then((rooms) => {
-							
-							setRooms(rooms);
-
-						})
-						.catch((error) => {
-
-                            
-
-						})
-
-					}}>
-						Create Room
-					</button>
-				</div>
-
-				<div 
-					className="rooms"
-					style={{
-						display:`flex`,
-						flexDirection:`column`,
-						overflow:`auto`
-					}}
-				>
-					{
-						rooms.map((room, index) => {
-
-							return (
-								<Room 
-									key={`room-${index}`}
-									room={room}
-									onJoin={() => {
-										
-										onJoinRoom(client, room);
-										
-									}} 
-									onLeave={() => {
-
-										onLeaveRoom(client, room);
-
-									}}
-								/>
-							);
-						})
-					}
-				</div>
-
-			</div>
-					
-			<div
-				style={{
-					display:`flex`,
-					flexDirection:`column`,
-					height:`100%`,
-					width:`66.6%`,
-					overflow:`hidden`
-				}}
-			>
-
-				<div style={{
-					display:`flex`,
-					flexDirection:`column`,
-					height:`33.3%`,
-					width:`100%`,
-					alignItems:`center`,
-					background:`darkgray`
-				}}>
-
-					<button 
-						onClick={() => {
-							
-							if (paused) {
-								client.current.resume()
-								.then((result) => {
-
-									if (
-										result && 
-										result.load && 
-										result.load.data && 
-										result.load.data.configured==="ok"
-									) {
-										setPaused(false);
-									}
-
-								});
-							} else {
-								client.current.pause()
-								.then((result) => {
-			
-									if (
-										result && 
-										result.load && 
-										result.load.data && 
-										result.load.data.configured==="ok"
-									) {
-										setPaused(true);
-									}
-										
-								});
-							}
-						}}
-						style={{
-							width:`100%`,
-							height:`21px`
-						}}
-					>
-						{paused ? 'Resume' : 'Pause'}
-					</button>
-
-					<button 
-						onClick={() => {
-
-							if (muted) {
-								client.current.unmute()
-								.then((result) => {
-
-									if (
-										result && 
-										result.load && 
-										result.load.data && 
-										result.load.data.configured==="ok"
-									) {
-										setMuted(false);
-									}
-
-								});
-							} else {
-								client.current.mute()
-								.then((result) => {
-			
-									if (
-										result && 
-										result.load && 
-										result.load.data && 
-										result.load.data.configured==="ok"
-									) {
-										setMuted(true);
-									}
-										
-								});
-							}
-
-						}}
-						style={{
-							width:`100%`,
-							height:`21px`
-						}}
-					>
-						{muted ? 'Unmute' : 'Mute'}
-					</button>
-
-					<button 
-						onClick={() => {
-
-							if (
-								client.current.publisher
-							) {
-								client.current.publisher.renegotiate({
-									audio: true,
-									video: true
-								});
-							}
-
-						}}
-						style={{
-							width:`100%`,
-							height:`21px`
-						}}
-					>
-						Renegotiate
-					</button>
-
-					<button 
-						onClick={() => {
-
-							clearInterval(client.current.keepAliveInterval);
-							
-						}}
-						style={{
-							width:`100%`,
-							height:`21px`
-						}}
-					>
-						Stop Keepalive
-					</button>
-
-					<div 
-						id="local"
-						style={{
-							display: `flex`,
-							maxHeight: `calc(100% - 84px)`,
-							width: `100%`,
-							alignItems: `center`,
-							justifyContent: `center`
-						}}
-					/>
-
-				</div>
-
-				<div
-					id="container"
-					style={{
-						display:`flex`,
-						flexWrap:`wrap`,
-						height:`66.6%`,
-						width:`100%`,
-						overflow:`auto`
-					}} 
-				/>
-
-			</div>
-			
+			{content}
 		</div>
 
-	);
+	}
 
+
+
+    render() {
+
+		if (!this.client) {
+			return null;
+		}
+		
+		return this.renderContainer();
+		
+	}
+	
 }
-
-
-export default VideoRoom
